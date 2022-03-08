c3f0d2607db71c2311952c1704f7fd5f0500122c
Sebastien Benard <github@deepnight.net>
2022-03-08 10:24:15 +0100

    Renamed ldtk

--- src/game/assets/World.hx
+++ src/game/assets/World.hx
@@ -1,4 +1,4 @@
 package assets;
 
 private typedef _Tmp =
-    haxe.macro.MacroType<[ ldtk.Project.build("res/levels/world.ldtk") ]>;
+    haxe.macro.MacroType<[ ldtk.Project.build("res/levels/paint_world.ldtk") ]>;
